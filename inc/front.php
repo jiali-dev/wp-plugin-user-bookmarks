1fc71b556e96ec271cbdaf5a96c887e3ecd74818
jiali <mahyarerad@gmail.com>
2025-04-15 02:30:40 +0330

    Modifying front UI

--- inc/front.php
+++ inc/front.php
@@ -10,7 +10,7 @@ function jialiufl_enqueue_assets() {
     wp_enqueue_style('jialiufl-fontawesome');
     wp_enqueue_script('jialiufl-fontawesome');
     wp_enqueue_style('jialiufl-styles');
-    wp_enqueue_script('jialiufl-main');
+    wp_enqueue_script('jialiufl-script');
 }
 
 /**
@@ -21,20 +21,34 @@ function jialiufl_enqueue_assets() {
  * @param array $enabled_for_fav
  * @return string
  */
-function jialiufl_get_buttons_html($post_type, $enabled_for_like, $enabled_for_fav) {
+function jialiufl_get_buttons_html() {
+    global $post;
+
+    if (!isset($post)) return '';
+
+    $post_type = get_post_type($post);
+
+    $enabled_for_like = get_option('jialiufl_enabled_post_types_for_like', []);
+    $enabled_for_fav  = get_option('jialiufl_enabled_post_types_for_fav', []);
+
+    // Exit early if not enabled for this post type
+    if (!in_array($post_type, $enabled_for_like) && !in_array($post_type, $enabled_for_fav)) {
+        return '';
+    }
+
     ob_start();
     ?>
-    <div class="jialiufl-favorite-and-like-button">
-        <?php if (in_array($post_type, $enabled_for_like)) : ?>
-            <span class="jiali-like-button">
+    <div class="jialiufl-favorite-and-like" data-post-id="<?php echo esc_attr($post->ID); ?>">
+        <?php if (in_array($post->post_type, $enabled_for_like)) : ?>
+            <span class="jialiufl-favorite-and-like-button" data-action="like">
                 <i class="fa-regular fa-heart"></i>
-                <span class="jiali-like-count">123156</span>
+                <span class="jialiufl-like-count"><?php $likes_count = jialiufl_get_likes_count($post->ID); echo ( $likes_count > 0 ? $likes_count : '' ); ?></span>
             </span>
         <?php endif; ?>
-        <?php if (in_array($post_type, $enabled_for_fav)) : ?>
-            <span class="jiali-favorite-button">
+        <?php if (in_array($post->post_type, $enabled_for_fav)) : ?>
+            <span class="jialiufl-favorite-and-like-button" data-action="favorite">
                 <i class="fa-regular fa-bookmark"></i>
-                <span class="jiali-favorite-count">11000</span>
+                <span class="jialiufl-favorite-count"><?php $favorites_count = jialiufl_get_post_favorites_count($post->ID); echo ( $favorites_count > 0 ? $favorites_count : '' ); ?></span>
             </span>
         <?php endif; ?>
     </div>
@@ -52,21 +66,31 @@ function jialiufl_append_buttons_to_content($content) {
     if (!is_singular()) return $content;
 
     global $post;
-    $post_type = get_post_type($post);
 
+    // Prevent double rendering if shortcode is used
+    if (has_shortcode($post->post_content, 'jialiufl_buttons')) {
+        return $content;
+    }
+
+    $post_type = get_post_type($post);
     $enabled_for_like = get_option('jialiufl_enabled_post_types_for_like', []);
     $enabled_for_fav  = get_option('jialiufl_enabled_post_types_for_fav', []);
 
-    // Exit early if nothing is enabled for this post type
+    // Exit early if not enabled
     if (!in_array($post_type, $enabled_for_like) && !in_array($post_type, $enabled_for_fav)) {
         return $content;
     }
 
     jialiufl_enqueue_assets();
 
-    $buttons_html = jialiufl_get_buttons_html($post_type, $enabled_for_like, $enabled_for_fav);
+    $buttons_html = jialiufl_get_buttons_html();
+
     $position = get_option('jialiufl_button_position', 'after');
 
-    return ($position === 'before') ? $buttons_html . $content : $content . $buttons_html;
+    if ($position === 'before') {
+        return $buttons_html . $content;
+    } else {
+        return $content . $buttons_html;
+    }
 }
 add_filter('the_content', 'jialiufl_append_buttons_to_content');
