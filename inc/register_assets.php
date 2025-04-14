97a70f9aec41944953c0dd8b9420e83febce2aa0
jiali <mahyarerad@gmail.com>
2025-04-15 02:33:12 +0330

    Localizing ajax script file

--- inc/register_assets.php
+++ inc/register_assets.php
@@ -12,7 +12,15 @@ function jialiufl_register_assets() {
     // Register styles
     wp_register_style('jialiufl-styles', JIALIUFL_CSS_URI . '/styles.css' , array(), '1.0.0', 'all');
     // Register scripts
-    wp_register_script('jialiufl-main', JIALIUFL_JS_URI . '/main.js', array('jquery'), '1.0.0', true);
+    wp_register_script('jialiufl-script', JIALIUFL_JS_URI . '/main.js', array('jquery'), '1.0.0', true);
+
+    // Localize script
+    wp_localize_script( 'jialiufl-script', 'jialiufl_ajax', 
+        array(
+            'ajaxurl' => admin_url('admin-ajax.php'),
+            'nonce' => wp_create_nonce('jialiufl-nonce')
+        )
+    );
 }
 add_action('wp_enqueue_scripts', 'jialiufl_register_assets');
 
