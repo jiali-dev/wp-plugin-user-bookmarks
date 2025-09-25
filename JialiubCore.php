eefa734f29422b6bf9e0c9b8f988d6ede971a953
jiali <mahyarerad@gmail.com>
2025-09-25 11:54:24 +0330

    Define Constants Without Class

--- JialiubCore.php
+++ JialiubCore.php
@@ -22,10 +22,6 @@ class JialiubCore {
         return self::$instance;
     }
 
-    private function defineConstants() {
-        JialiubDefineConstants::defien();
-    }
-
     private function registerAutoload() {
         spl_autoload_register(function ($class_name) {
             // Only autoload classes starting with "Ab"
@@ -36,6 +32,19 @@ class JialiubCore {
                 }
             }
         });
+    }
+
+    private function defineConstants() {
+        define('JIALIUB_PLUGIN_PATH', plugin_dir_path(__FILE__));
+        define('JIALIUB_PLUGIN_URL', plugin_dir_url(__FILE__));
+        define('JIALIUB_CLASSES_PATH', JIALIUB_PLUGIN_PATH . 'classes/');
+        define('JIALIUB_ASSETS_URI', JIALIUB_PLUGIN_URL . 'assets');
+        define('JIALIUB_CSS_URI', JIALIUB_ASSETS_URI . 'css');
+        define('JIALIUB_JS_URI', JIALIUB_ASSETS_URI . 'js');
+        $singular = get_option('jialiub_singular_label');
+        $plural = get_option('jialiub_plural_label');
+        define('JIALIUB_SINGULAR_LABEL', !empty($singular) ? $singular : esc_html__( 'Bookmark', 'jiali-user-bookmarks' ) );
+        define('JIALIUB_PLURAL_LABEL', !empty($plural) ? $plural : esc_html__( 'Bookmarks', 'jiali-user-bookmarks' ) );
     }
 
     private function init() {
