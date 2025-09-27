50468db50baa4f6ea4fbf57227624423141f6c5b
jiali <mahyarerad@gmail.com>
2025-09-27 13:42:04 +0330

    Enhance user bookmarks table

--- assets/plugins/datatable/datatable-custom.js
+++ assets/plugins/datatable/datatable-custom.js
@@ -2,13 +2,28 @@ jQuery(function ($) {
   "use strict";
 
   let table = new DataTable(".jialiub-bookmarks-table", {
+    processing: true,
+    serverSide: true,
+    ajax: {
+        url: jialiub_ajax.ajaxurl,
+        type: 'POST',
+        data: function(d){
+            d.action = 'jialiub_get_user_bookmarks_ajax';
+            d.nonce = jialiub_ajax.nonce;
+        }
+    },
+    columns: [
+        { title: "Title" },
+        { title: "Author" },
+    ],
+    pageLength: 10,
+    lengthChange: false,
     responsive: true,
-    pageLength: 10, // how many rows per page
     lengthMenu: [
       [10, 25, 50, -1],
       [10, 25, 50, "All"],
     ], // dropdown for rows
-    paging: false, // enable pagination
+    paging: true, // enable pagination
     searching: true, // optional: enable search
     ordering: true, // optional: enable sorting
   });
